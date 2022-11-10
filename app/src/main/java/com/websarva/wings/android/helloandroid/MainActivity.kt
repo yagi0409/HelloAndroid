cc2f17142eff1c5a07ebc80fd5e17bfae5222ed4
青柳奏汰 <greenwillow49@gmail.com>
2022-11-10 22:45:58 +0900

    Finish

--- app/src/main/java/com/websarva/wings/android/helloandroid/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/helloandroid/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.websarva.wings.android.helloandroid
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,26 +12,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btClick = findViewById<Button>(R.id.btClick)
-
         val listener = HelloListener()
-
         btClick.setOnClickListener(listener)
 
         val btClear = findViewById<Button>(R.id.btClear)
-
         btClear.setOnClickListener(listener)
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        val radio = RadioListener()
+        radioGroup.setOnCheckedChangeListener(radio)
+    }
+
+    var gender = ""
+
+    private inner class RadioListener: RadioGroup.OnCheckedChangeListener{
+        override fun onCheckedChanged(rg: RadioGroup, checkedRadioButtonId: Int) {
+            val radioButton = findViewById<RadioButton>(checkedRadioButtonId)
+
+            when(radioButton.text){
+                "男" -> {
+                    gender = "Mr."
+                }
+                "女" -> {
+                    gender = "Ms."
+                }
+            }
+        }
+
     }
 
     private inner class HelloListener: View.OnClickListener{
         override fun onClick(view: View) {
             val input = findViewById<EditText>(R.id.etName)
-            val raddioGroup = findViewById<RadioGroup>(R.id.)
             val output = findViewById<TextView>(R.id.tvOutput)
 
             when(view.id){
                 R.id.btClick -> {
                     val inputStr = input.text.toString()
-                    output.text = "Welcome to " + inputStr + " さん！"
+                    when(inputStr) {
+                        "" -> {
+                            output.text = "お名前を入力してください！"
+                        }
+                        else -> {
+                            output.text = "Welcome to " + gender + inputStr + "!!!"
+                        }
+                    }
                 }
 
                 R.id.btClear -> {
